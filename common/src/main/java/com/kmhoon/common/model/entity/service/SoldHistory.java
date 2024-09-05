d188a7e513f58b2b6c7cd6a2cc759815d8f5936c
K-MHOON <azmria117@gmail.com>
2024-09-05 23:06:55 +0900

    feat : 판매이력 엔티티 추가

---
+++ common/src/main/java/com/kmhoon/common/model/entity/service/SoldHistory.java
@@ -0,0 +1,37 @@
+package com.kmhoon.common.model.entity.service;
+
+import com.kmhoon.common.enums.PaymentMethod;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+import java.time.LocalDateTime;
+
+/**
+ * 자판기 물품 판매 정보
+ */
+@Entity
+@Table(name = "tb_service_sold_history")
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class SoldHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long sequence;
+
+    @Comment("판매시간")
+    private LocalDateTime soldTime;
+
+    @Comment("판매된시점의가격")
+    private long price;
+
+    @Comment("결제방식")
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private VendingMachineItemMap vendingMachineItemMap;
+}
