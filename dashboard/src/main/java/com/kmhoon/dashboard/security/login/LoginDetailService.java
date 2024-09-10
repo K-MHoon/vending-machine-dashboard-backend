ce0272a67dcb3d21967d228d2cf11071971243a0
K-MHOON <azmria117@gmail.com>
2024-09-10 23:01:16 +0900

    feat : LoginDetailService 추가

---
+++ dashboard/src/main/java/com/kmhoon/dashboard/security/login/LoginDetailService.java
@@ -0,0 +1,44 @@
+package com.kmhoon.dashboard.security.login;
+
+import com.kmhoon.common.model.entity.auth.user.User;
+import com.kmhoon.common.repository.auth.user.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class LoginDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmailWithRoles(username.strip()).orElseThrow(() -> new EntityNotFoundException("해당하는 이메일이 존재하지 않습니다."));
+
+        if(!user.getIsUse()) {
+            throw new EntityNotFoundException("사용 중지된 사용자 입니다. 관리자에게 문의하십시오.");
+        }
+
+        if(user.getIsLock()) {
+            throw new EntityNotFoundException("잠긴 사용자 입니다. 관리자에게 문의하십시오.");
+        }
+
+        List<SimpleGrantedAuthority> authorityList = user.getUserRoleList().stream()
+                .map(u -> new SimpleGrantedAuthority(u.getRole().getName()))
+                .collect(Collectors.toList());
+
+        return new UserInfo(user.getEmail(), user.getName(), user.getPassword(), authorityList);
+    }
+
+
+}
