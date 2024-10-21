d5d6b0b520517b9a34a58332c67f2727eed0b4ec
K-MHOON <azmria117@gmail.com>
2024-10-21 22:53:30 +0900

    feat : Vendor 생성 Dto 추가

---
+++ common/src/main/java/com/kmhoon/common/model/dto/service/vendor/CreateVendorRequest.java
@@ -0,0 +1,21 @@
+package com.kmhoon.common.model.dto.service.vendor;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CreateVendorRequest {
+
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String phoneNumber;
+    @NotBlank
+    private String address;
+    @NotNull
+    private Boolean isUse;
+}
