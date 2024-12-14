78ff4fc8531eda6824c0ed9de83e78d7287d6909
K-MHOON <azmria117@gmail.com>
2024-12-14 22:31:09 +0900

    feat : 자판기 수정 컨트롤러, 에러코드 추가

--- dashboard/src/main/java/com/kmhoon/dashboard/controller/vendingMachine/VendingMachineController.java
+++ dashboard/src/main/java/com/kmhoon/dashboard/controller/vendingMachine/VendingMachineController.java
@@ -1,6 +1,7 @@
-package com.kmhoon.dashboard.controller;
+package com.kmhoon.dashboard.controller.vendingMachine;
 
 import com.kmhoon.common.model.dto.service.vendingMachine.CreateVendingMachineRequest;
+import com.kmhoon.common.model.dto.service.vendingMachine.UpdateVendingMachineRequest;
 import com.kmhoon.dashboard.service.vendingMachine.VendingMachineService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/service")
+@RequestMapping("/api/service/vending-machine")
 public class VendingMachineController {
 
     private final VendingMachineService service;
@@ -18,5 +19,11 @@ public class VendingMachineController {
     @ResponseStatus(HttpStatus.OK)
     public void createVendingMachine(@RequestBody @Validated CreateVendingMachineRequest request) {
         service.createVendingMachine(request);
+    }
+
+    @PutMapping("/vending-machine")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateVendingMachine(@RequestBody @Validated UpdateVendingMachineRequest request) {
+        service.updateVendingMachine(request);
     }
 }
--- dashboard/src/main/java/com/kmhoon/dashboard/exception/service/vendingMachine/VendingMachineErrorCode.java
+++ dashboard/src/main/java/com/kmhoon/dashboard/exception/service/vendingMachine/VendingMachineErrorCode.java
@@ -14,6 +14,7 @@ public enum VendingMachineErrorCode implements ErrorCode {
     VENDOR_SEQ_NOT_FOUND("S-VENDINGMACHINE-003", HttpStatus.BAD_REQUEST, "해당하는 자판기 제조사를 찾을 수 없습니다."),
     ENGINEER_SEQ_NOT_FOUND("S-VENDINGMACHINE-004", HttpStatus.BAD_REQUEST, "해당하는 엔지니어를 찾을 수 없습니다."),
     DELIVERY_MAN_SEQ_NOT_FOUND("S-VENDINGMACHINE-005", HttpStatus.BAD_REQUEST, "해당하는 배달기사를 찾을 수 없습니다."),
+    VENDING_MACHINE_NOT_FOUND("S-VENDINGMACHINE-006", HttpStatus.BAD_REQUEST, "해당하는 자판기 정보를 찾을 수 없습니다."),
 
     ;
 
