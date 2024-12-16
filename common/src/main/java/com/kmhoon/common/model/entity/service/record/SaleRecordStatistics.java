e8c6ae53e304fa237e4a4c59fd36cbe80758bdef
K-MHOON <azmria117@gmail.com>
2024-12-16 23:45:14 +0900

    feat : 자판기 매출 요약 정보 엔티티 추가

---
+++ common/src/main/java/com/kmhoon/common/model/entity/service/record/SaleRecordStatistics.java
@@ -0,0 +1,32 @@
+package com.kmhoon.common.model.entity.service.record;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+import java.time.LocalDate;
+
+/**
+ * 자판기 물품 매출 요약 정보
+ */
+@Entity
+@Table(name = "tb_service_sale_record_statistics")
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class SaleRecordStatistics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long sequence;
+
+    @Comment("보고서요약관련월")
+    private LocalDate reportDate;
+
+    @Comment("전체매출")
+    private Long totalRevenue;
+
+    @Comment("전월대비매출증감율")
+    private Double mom;
+}
